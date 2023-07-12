a2d4ef7bbc3ccbf5be11197734d7d7978b4d8139
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-12 22:31:17 +0330

    Update Session_String.py

--- Session_String.py
+++ Session_String.py
@@ -3,6 +3,8 @@ from telethon.sync import TelegramClient
 api_id = 12345  # Replace with your API ID
 api_hash = 'YOUR_API_HASH'
 phone_number = '+1234567890'  # Replace with your phone number
+session_file = 'my_session.session'  # Replace with your desired session file name
 
-with TelegramClient(None, api_id, api_hash) as client:
-    print(client.session.save())
+with TelegramClient(session_file, api_id, api_hash) as client:
+    client.start(phone_number)
+    print("Session string saved to:", session_file)
